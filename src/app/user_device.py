fe8a782b1041d6c4965dd138530ea504a60d93f6
bmpang <bmpang222@gmail.com>
2025-12-06 17:24:28 -0800

    add route for timing download of file direct

--- src/app/user_device.py
+++ src/app/user_device.py
@@ -1,4 +1,7 @@
 import requests
+import time
+import zipfile
+from io import BytesIO
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -10,10 +13,55 @@ def heartbeat():
     return jsonify({"status": "ok", "message": "Flask heartbeat OK"}), 200
 
 
+@app.route("/download_direct")
+def download_direct():
+    target_url = "https://github.com/twbs/bootstrap/archive/v4.4.1.zip"
+
+    try:
+        start_time = time.time()
+        response = requests.get(target_url)
+        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+
+        _zip = BytesIO(response.content)
+
+        with zipfile.ZipFile(_zip, 'r') as zf:
+            files = []
+
+            for name in zf.namelist():
+                try:
+                    file_content = zf.read(name).decode('cp437')
+                    files.append(file_content)
+                except KeyError:
+                    print(name + " not found in the archive.")
+
+            jsonify(files)
+
+            elapsed_time = time.time() - start_time
+
+            return jsonify(str(elapsed_time*1000) + " milliseconds")
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/download_direct_cdn")
+def download_cdn():
+    target_url = "https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+
+    return get(target_url)
+
 @app.route("/send_request")
 def send_request():
     target_url = request.args.get("target_url")
 
+    try:
+        response = requests.get(target_url)
+        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+        data = response.json()
+        return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+
+def get(target_url):
     try:
         response = requests.get(target_url)
         response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
